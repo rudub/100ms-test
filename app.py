870baafee2f934d4184696841d0c3c7b1e80a17a
rudub <35490403+rudub@users.noreply.github.com>
2021-07-18 23:32:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 def print_query():
     args_passed = request.args
     return args_passed
-    #return '''<h1>The language is : {}</h1>'''.format(language)
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0",  port=5000, debug=True)
